785039addcb055d275f5377d9c8d3304cd7d0075
goranpejcic <gpejcic89@hotmail.com>
2024-06-30 12:25:04 +0200

    End of Section 3

--- Reactivities/Program.cs
+++ Reactivities/Program.cs
@@ -20,6 +20,13 @@ namespace Reactivities
             {
                 opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
             });
+            builder.Services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+                });
+            });
 
             var app = builder.Build();
 
@@ -31,6 +38,8 @@ namespace Reactivities
             }
 
             app.UseHttpsRedirection();
+
+            app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
 
