b079a011183ae6308fccde1d0eb740a3433aade1
goranpejcic <gpejcic89@hotmail.com>
2024-07-16 13:48:28 +0200

    1. Comment what happens in each line in app.tsx 2. Create input and button from SemanticUI. When the button is pressed an API should be called with the guid from the input field. As a result we get a single activity in a list. 3. Replace the FindAsync method in the endpoint that gets the single activity with for or foreach loop.

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -4,6 +4,8 @@ import './App.css';
 import axios from 'axios';
 import { Header, List } from 'semantic-ui-react';
 
+// pravimo novi interface koji je preslikan onom sa backend-a, kako bi znali 
+// kog ce to zapravo biti tipa promenjiva activity
 interface Activity {
   id: string;
   title: string;
@@ -15,7 +17,21 @@ interface Activity {
 }
 
 function App() {
+
+  // sa useState postavljamo promenjive stanja i funkcije koje ce da hvataju tu promenu
+  // kada se neka od njih promeni, to je znak da cela komponenta (App) treba da se rerenderuje
+  // ali React to optimizuje pa update-uje samo deo koji se zapravo promenio
+  
   const [activity, setActivity] = useState<Activity | null>(null);
+  // pravimo promenjivu activity koja moze biti tipa Activity ili null, i inicijalno je postavljamo na null
+  // "pravimo" i "kao neku funkciju" koja nam sluzi za setovanje gore pomenute promenjive
+  // sta tacno radi useState = ?, ali ocigledno kada pomocu toga uvedemo promenjive
+  // to nam stvara preduslove da se posle pri menjanu stanja (vrednosti) te promenjive
+  // rerenderuje samo taj deo gde se ona zapravo koristi => ne mora cela f-ja npr. ne mora
+  // ceo app.tsx da se rerenderuje ako promenimo vrednost activity promenjive
+
+  
+  // u useEffect je zapravo ono sto ce da se promeni kada se komponenta rerenderuje
 
   useEffect(() => {
     axios.get('https://localhost:7266/api/activities/fdb5ddfd-8836-453d-3889-08dca0cd6879').then(response => {
@@ -23,10 +39,17 @@ function App() {
       setActivity(response.data);
     })
   }, [])
+  // sa axios.get gadjamo backend i u njemu odredjeni endpoint. Axios nam vraca "promise" (sta god to bilo, ali
+  // to nam omogucava da mozemo da vezujemo komande jednu za drugom)
+  // dalje, dobijamo neki response, koji prvo logujemo u konzolu, a zatim u activity promenjivu
+  // postavljamo vrednost koja se nalazi u response.data
+  // na kraju, moramo da stavimo prazan niz => od cega nam zavisi da li ce se ovo sve pozvati? ni od cega
+  // ali hocemo da se pozove jednom na pocetku i to se radi tako sto se prosledi prazan niz kao argument od useEffect
+
 
   return (
     <div>
-      {activity && (
+      {activity && ( // prikazuje se samo ako activity nije null, jer samo tada mozemo da koristimo activity.id, title itd.
         <div>
           <Header as='h2' icon='users' content='Reactivities'/>
             <List>
