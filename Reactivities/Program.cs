24ed283fac9544791ac33cbffd731b9e13b9ebdb
goranpejcic <gpejcic89@hotmail.com>
2024-07-20 15:40:34 +0200

    End of Section 4

--- Reactivities/Program.cs
+++ Reactivities/Program.cs
@@ -1,5 +1,7 @@
 
+using API.Extensions;
 using Application.Activities;
+using Application.Core;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 using Persistance;
@@ -15,21 +17,7 @@ namespace Reactivities
             // Add services to the container.
 
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-            builder.Services.AddDbContext<DataContext>(opt =>
-            {
-                opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-            });
-            builder.Services.AddCors(opt =>
-            {
-                opt.AddPolicy("CorsPolicy", policy =>
-                {
-                    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
-                });
-            });
-            builder.Services.AddMediatR(typeof(List.Handler).Assembly);
+            builder.Services.AddApplicationServices(builder.Configuration);
 
             var app = builder.Build();
 
